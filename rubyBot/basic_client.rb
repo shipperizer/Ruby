096b75f3b0826fb3ec20983aa4ab67e1f809736e
shipperizer <alexcabb@gmail.com>
2013-10-08 17:01:14 +0200

    Ruby various

---
+++ rubyBot/basic_client.rb
@@ -0,0 +1,11 @@
+require './bot.rb'
+
+bot = Bot.new(:name => ARGV[0], :data_file => ARGV[1])
+
+puts bot.greeting
+
+while input = $stdin.gets and input.chomp != 'end'
+  puts '>> ' + bot.response_to(input)
+end
+
+puts bot.farewell
