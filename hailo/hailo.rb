ec59dbaec2790fe8cd0e5a2667f4becd3640eaa1
shipperizer <alexcabb@gmail.com>
2014-02-03 12:09:10 +0000

    Update hailo.rb

--- hailo/hailo.rb
+++ hailo/hailo.rb
@@ -2,54 +2,97 @@
 #Abnormal is defined - a distance between two points
 #that cannot be reached without speeding over 40.
 
+class GeoPoint 
+
+	def initialize(latitude, longitude, timestamp)
+		@latitude = latitude.to_f
+		@longitude = longitude.to_f
+		@timestamp = timestamp.to_i
+	end
+
+	def getLat
+		@latitude		
+	end
+
+	def getLong
+		@longitude
+	end
+
+	def getTime
+		@timestamp
+	end
+end
+
+class Path
+
+	@@earthRad=6371
+
+	def initialize
+		@path=[]
+	end
+
+	def add(geoPoint)
+		@path << geoPoint
+	end
+
+	def filter(speedlimit)
+		puts "before filtering #{@path.count} \n #{@path.inspect}"
+		redundant=[]
+		for i in 1..@path.count-1
+			space = distance(@path[i-1],@path[i])
+			time=@path[i].getTime-@path[i-1].getTime
+			speed=(space/time)*3600
+			#1 (metre per second) = 3.6 km per hour
+			if speed > speedlimit
+				redundant << i
+			end
+		end
+		
+		redundant.each do |i|
+			@path[i]=nil
+		end 
+		@path.compact!		
+		
+		puts "after filtering #{@path.count} \n #{@path.inspect}"
+	end
+
+	private
+
+	def distance(pointA , pointB)
+		dLat=(pointB.getLat-pointA.getLat)*(Math::PI/180)
+		dLon=(pointB.getLong-pointA.getLong)*(Math::PI/180)
+		a= Math::sin(dLat/2) * Math::sin(dLat/2) + Math::cos(pointA.getLat*Math::PI/180) * Math::cos(pointB.getLat*Math::PI/180) * Math::sin(dLon/2) * Math::sin(dLon/2)
+		b= 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+		return @@earthRad * b
+	end
+end
+
+
 
 class Hailo
-	attr_accessor :coordinates
-	def initialize(file_path)
-		@coordinates=[]
-		@timestamp=[]
+	
+	def initialize(file_path='points.csv')
+		@path=Path.new
 		arrayer file_path
 	end
 
 	def arrayer(file_path)
-		a=0
 		File.open(file_path, 'r') do |file|
     		file.each do |line|
       			tmp=line.split(',')
-      			@coordinates[a]=[tmp[0].to_f,tmp[1].to_f]
-      			@timestamp[a]=tmp[2].to_i
-      			a+=1	
-    		end
-    	end
+      			@path.add(GeoPoint.new(tmp[0],tmp[1],tmp[2]))
+      		end
+    	end    	
   	end
 
-	def distance(pointA , pointB)
-		earthRad=6371
-		dLat=(pointB[0]-pointA[0])*(Math::PI/180)
-		dLon=(pointB[1]-pointA[1])*(Math::PI/180)
-		a= Math::sin(dLat/2) * Math::sin(dLat/2) + Math::cos(pointA[0]*Math::PI/180) * Math::cos(pointB[0]*Math::PI/180) * Math::sin(dLon/2) * Math::sin(dLon/2)
-		b= 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
-		return earthRad * b
-	end
-
-	def filter
-		result=[]
-		result<<[@coordinates[0][0],@coordinates[0][1],@timestamp[0]]
-		for i in 1..@coordinates.count-1
-			space=distance(@coordinates[i-1],@coordinates[i])
-			time=@timestamp[i]-@timestamp[i-1]
-			speed=(space/time)*3600
-			#1 (metre per second) = 3.6 km per hour
-			if speed < 40
-				result << [@coordinates[i][0],@coordinates[i][1],@timestamp[i]]
-			end
-		end
-		result
+	def filter(speedlimit=30)
+		@path.filter(speedlimit)
+		@path
 	end
 end
 
-test=Hailo.new('points.csv')
+test=Hailo.new(ARGV[0])
 res=test.filter
-puts res
+puts "MAIN after filtering \n #{res.inspect}"
 
 #use of this: ruby hailo.rb > file
